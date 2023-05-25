72086f0982647e67b1b5961ea631ad8eba3c8429
EC2 Default User <ec2-user@ip-172-31-41-206.ap-northeast-1.compute.internal>
2023-05-25 14:01:56 +0000

    [Finish]nagano_cake

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ devise_for :customers,skip: [:passwords], controllers: {
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
+  scope module: :public do
+    get 'customers/my_page' => 'customers#show', as: 'my_page'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
