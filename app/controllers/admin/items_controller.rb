57527eaefe7d583f4f784f594b0678330a5c9077
EC2 Default User <ec2-user@ip-172-31-41-206.ap-northeast-1.compute.internal>
2023-06-07 13:57:28 +0000

    [商品編集]nagano_cake

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -12,8 +12,25 @@ class Admin::ItemsController < ApplicationController
     end
   end
 
+  def index
+    @item = Item.page(params[:page])
+  end
+
   def show
     @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   private
