82f9eba1609a53966c9a38bd18bd02dbb45aeb7f
EC2 Default User <ec2-user@ip-172-31-41-206.ap-northeast-1.compute.internal>
2023-06-14 13:50:43 +0000

    [カート機能]nagano_cake

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,20 @@
 class Public::CartItemsController < ApplicationController
+
+  def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
+  end
+
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save
+    redirect_to cart_items_path
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :quantity)
+  end
 end
