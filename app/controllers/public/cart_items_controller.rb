194335ecb9df69f35f8ae19505ab7988d38b86fd
EC2 Default User <ec2-user@ip-172-31-41-206.ap-northeast-1.compute.internal>
2023-06-16 13:00:57 +0000

    [カート機能]nagano_cake

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -16,6 +16,15 @@ class Public::CartItemsController < ApplicationController
     redirect_to cart_items_path
   end
 
+  def update
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      redirect_to cart_items_path
+    else
+      render :index
+    end
+  end
+
   private
 
   def cart_item_params
