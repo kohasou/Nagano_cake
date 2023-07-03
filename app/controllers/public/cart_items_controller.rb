6f8e64b132dea5dc3ab1fb7e262783ab64f9a1a3
EC2 Default User <ec2-user@ip-172-31-41-206.ap-northeast-1.compute.internal>
2023-07-03 13:28:40 +0000

    [カート昨日調整]nagano_cake

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -11,10 +11,16 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    @cart_item.save
-    redirect_to cart_items_path
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      @cart_item.quantity += params[:cart_item][:quantity].to_i
+      @cart_item.save
+    else
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.customer_id = current_customer.id
+      @cart_item.save
+    end
+      redirect_to cart_items_path
   end
 
   def update
